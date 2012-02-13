caebbeac53953ec7f3f5ac4cea35cf981091fa9e
ddtddt <ddtddt@piwigo.org>
2012-02-13 13:58:31 +0000

    [extensions] - Subscribe_to_comments - add pl_PL - Polski - Polish - Thanks to kuba
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@13141 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+ $lang['Please check your email inbox to confirm your subscription.'] = 'Sprawdź skrzynką pocztową aby potwierdzić subskrypcje.';
+ $lang['You have been added to the list of subscribers for this album.'] = 'Zostałeś dodany do listy subskrybentów tego albumu.';
+ $lang['You have been added to the list of subscribers for this picture.'] = 'Zostałeś dodany do listy subskrybentów tego zdjęcia.';
+ $lang['Invalid email adress, your are not subscribed to comments.'] = 'Niewłaściwy adres e-mail, nie zostałeś subskrybowany.';
+ $lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Usunąłeś ten adres e-mail z listy subskrybentów.';
+ $lang['You are currently subscribed to comments.'] = 'Jesteś obecnie zasubskrybowany do komentarzy.';
+ $lang['Unsubscribe'] = 'Nie subskrybuj';
+ $lang['Subscribe'] = 'Subskrybuj';
+ $lang['Subscribe without commenting'] = 'Subskrybuj bez komentowania.';
+ $lang['Notify me of followup comments'] = 'Powiadamiaj o odpowiedziach.';
+ $lang['Invalid email adress.'] = 'Nieprawidłowy adres e-mail.';
+ $lang['Your subscribtion has been validated, thanks you.'] = 'Twoja subskrypcja została potwierdzona, dziękujemy.';
+ $lang['Nothing to validate.'] = 'Nie ma czego potwierdzać.';
+ $lang['Subscriptions of'] = 'Subskrypcje';
+ $lang['Unsubscribe from email notification'] = 'Zakończ subskrypcję powiadomień e-mail';
+ $lang['Only unsubscribe notifications for comments from:'] = 'Zakończ tylko subskrypcję powiadomień komentarzy od:';
+ $lang['Unsubscribe from all email notifications'] = 'Zakończ subskrypcję wszystkich powiadomień';
+ $lang['Manage my subscriptions to comments'] = 'Zarządzaj moimi subskrypcjami komentarzy';
+ $lang['Item'] = 'Element';
+ $lang['Date'] = 'Data';
+ $lang['You are not subscribed to any comment.'] = 'Nie subskrybowałeś żadnych komentarzy.';
+ $lang['Return to item page'] = 'Powróć do listy elementów'; 
+?>
