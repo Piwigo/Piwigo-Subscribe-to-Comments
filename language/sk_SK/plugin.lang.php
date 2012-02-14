58e30c98e6f92990854c98be69910623e64be73f
ddtddt <ddtddt@piwigo.org>
2012-02-14 20:37:54 +0000

    [extensions] - Subscribe_to_comments - add sk_SK – Slovensky – Slovak - Thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@13164 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Please check your email inbox to confirm your subscription.'] = 'Prosím skontrolujte si svoj mail na potvrdenie odberu.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Boli ste pridaný do zoznamu odberov tohto albumu.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Boli ste pridaný do zoznamu odberov tejto fotky.';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Neplatná emailová adresa, neboli ste zapísaný do odberu komentárov.';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Úspešné odhlásenie Vašej emailovej adresy z prijímania upozornení.';
+$lang['You are currently subscribed to comments.'] = 'Práve ste zapísaný do odberu komentárov.';
+$lang['Unsubscribe'] = 'Odhlásiť';
+$lang['Subscribe'] = 'Prihlásiť';
+$lang['Subscribe without commenting'] = 'Prihlásiť bez komentovania';
+$lang['Notify me of followup comments'] = 'Upozorniť ma na nasledovné komentáre';
+$lang['Invalid email adress.'] = 'Neplatná emailová adresa.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Vaše prihlásenie bolo potvrdené, ďakujeme.';
+$lang['Nothing to validate.'] = 'Nepotvrdené.';
+$lang['Subscriptions of'] = 'Prihlásený';
+$lang['Unsubscribe from email notification'] = 'Odhlásiť z emailovej notifikácie';
+$lang['Only unsubscribe notifications for comments from:'] = 'Len odhlásenie notifikácií pre komentére z:';
+$lang['Unsubscribe from all email notifications'] = 'Odhlásenie zo všetkých emailových notofikácií';
+$lang['Manage my subscriptions to comments'] = 'Ovládanie mojich prihlásení na odber komentárov';
+$lang['Item'] = 'Položka';
+$lang['Date'] = 'Dátum';
+$lang['You are not subscribed to any comment.'] = 'Nie ste prihlásený na odber žiadneho komentára.';
+$lang['Return to item page'] = 'Návrat na stránku položiek';
+?>
