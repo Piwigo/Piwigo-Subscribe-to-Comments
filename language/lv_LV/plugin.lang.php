81fcaeadf74760e2d755248a9dcdfb36184ff9c2
ddtddt <ddtddt@piwigo.org>
2012-02-24 16:24:56 +0000

    [extensions] - Subscribe_to_comments - add lv_LV - Latviešu - Latvian - Thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@13326 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please check your email inbox to confirm your subscription.'] = 'Lūdzu pārbaudiet savu e-pastu, lai apstiprinātu savu pierakstu.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Jūs esat pievienots šā albūma parakstītāju sarakstam.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Jūs esat pievienots šā foto parakstītāju sarakstam.';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Nekorekta e-pasta adrese, jūs neesat pierakstījies komentāriem.';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Jūsu e-pasta adrese sekmīgi izņemta no paziņojumu saņēmēju saraksta.';
+$lang['You are currently subscribed to comments.'] = 'Jūs patlaban esat pierakstījies komentāriem.';
+$lang['Unsubscribe'] = 'Atsacīties no pieraksta';
+$lang['Subscribe'] = 'Pierakstīties';
+$lang['Subscribe without commenting'] = 'Pierakstīties nekomentējot';
+$lang['Notify me of followup comments'] = 'Paziņot man par tekošajiem komentāriem';
+$lang['Invalid email adress.'] = 'Nekorekta e-pasta adrese.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Paldies. Jūsu pieraksts ir validēts.';
+$lang['Nothing to validate.'] = 'Nav ko validēt.';
+$lang['Subscriptions of'] = 'Pierakstīšanās';
+$lang['Unsubscribe from email notification'] = 'Atsacītiess no e-pasta paziņojumiem';
+$lang['Only unsubscribe notifications for comments from:'] = 'Atsacīties tikai no paziņojumiem par komentāriem no:';
+$lang['Unsubscribe from all email notifications'] = 'Atsacīties no visiem epasta paziņojumiem';
+$lang['Manage my subscriptions to comments'] = 'Pārvaldīt manus paziņojumus par komentāriem';
+$lang['Item'] = 'Vienums';
+$lang['Date'] = 'Datums';
+$lang['You are not subscribed to any comment.'] = 'Jūs neesat pierakstījies jebkādu komentāru veikšanai.';
+$lang['Return to item page'] = 'Atgriezties vienuma lapā';
+?>
