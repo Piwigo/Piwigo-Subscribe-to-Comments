a5869b4e573380a3c0bff85ef29a08e0f54c9db1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-26 07:42:47 +0000

    [Subscribe to Comments] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@13364 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Date'] = 'Дата';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Невірна електронна адреса, Ви не підписані на коментарі.';
+$lang['Invalid email adress.'] = 'Недійсна адреса електронної пошти.';
+$lang['Item'] = 'Елемент';
+$lang['Manage my subscriptions to comments'] = 'Управління підпискою на мої коментарі';
+$lang['Nothing to validate.'] = 'Нічого для підтвердження.';
+$lang['Notify me of followup comments'] = 'Повідомити мене про нові коментарі';
+$lang['Only unsubscribe notifications for comments from:'] = 'Лише відписатись від сповіщення про коментарі від:';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Будь ласка, перевірте вашу поштову скриньку, щоб підтвердити підписку.';
+$lang['Return to item page'] = 'Повернутися на сторінку елемента';
+$lang['Subscribe'] = 'Підписатися';
+$lang['Subscribe without commenting'] = 'Підписатися не коментуючи';
+$lang['Subscriptions of'] = 'Підписка на';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Ви успішно відписалися від отримання сповіщень.';
+$lang['Unsubscribe'] = 'Відписатися від підписки';
+$lang['Unsubscribe from all email notifications'] = 'Відписатись від усіх поштових повідомлень';
+$lang['Unsubscribe from email notification'] = 'Відписатись від повідомлень по електронній пошті';
+$lang['You are currently subscribed to comments.'] = 'Ви підписані на коментарі.';
+$lang['You are not subscribed to any comment.'] = 'Ви не підписані на жоден коментар.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Ви були додані до списку передплатників для цього альбому.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Ви були додані до списку передплатників для цього зображення.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Ваша підписка була підтверджена, спасибі вам.';
+?>
