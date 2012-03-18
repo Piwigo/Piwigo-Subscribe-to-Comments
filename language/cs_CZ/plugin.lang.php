87bb04b3118940471af8e89b8f29ac72a4aa12e2
ddtddt <ddtddt@piwigo.org>
2012-03-18 12:21:24 +0000

    [extensions] - Subscribe_to_comments - add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@13606 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+$lang['Please check your email inbox to confirm your subscription.'] = 'Please check your email inbox to confirm your subscription.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Byl jste přidán do seznamu účastníků diskuze pro toto album.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Byl jste přidán do seznamu účastníků diskuze pro tento obrázek.';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Byla zadána neplatná email adresa, nebyl jste zařazen k odběru komentářů.';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Vaše email adresa byla úspěšně odstraněna ze seznamu odběratelů pro upozorňující zprávy.';
+$lang['You are currently subscribed to comments.'] = 'Nyní jste přihlášen k odběru komentářů.';
+$lang['Unsubscribe'] = 'Odhlásit';
+$lang['Subscribe'] = 'Přihlásit';
+$lang['Subscribe without commenting'] = 'Přihlášení bez zadání komentáře';
+$lang['Notify me of followup comments'] = 'Upozorni mě na následující komentáře';
+$lang['Invalid email adress.'] = 'Neplatný email.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Vaše přihlášení k odběru bylo potvrzeno, děkujeme vám.';
+$lang['Nothing to validate.'] = 'Nic k ověření.';
+$lang['Subscriptions of'] = 'Odběr z';
+$lang['Unsubscribe from email notification'] = 'Odhlásit se z odběru upozorňujících zpráv o nových komentářích';
+$lang['Only unsubscribe notifications for comments from:'] = 'Odhlásit se z odběru upozornění jen z těchto komentářů:';
+$lang['Unsubscribe from all email notifications'] = 'Odhlásit se z odběru upozornění ze všech komentářů';
+$lang['Manage my subscriptions to comments'] = 'Spravovat moje přihlášení k odběrům komentářů';
+$lang['Item'] = 'Zpráva';
+$lang['Date'] = 'Datum';
+$lang['You are not subscribed to any comment.'] = 'Nejste nyní přihlášen k oběru upozornění u žádného komentáře.';
+$lang['Return to item page'] = 'Zpět na stránku zprávy';
+?>
