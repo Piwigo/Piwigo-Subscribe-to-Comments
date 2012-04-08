73cc608753f9c948e7a320d5a2a4caeb2cbc8226
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-08 20:40:58 +0000

    [Subscribe to Comments] Add pt_PT, thanks to : PsyKick_RuhYn
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@14013 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Date'] = 'Data';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Endereço de correio electrónico inválido, não foi subscrito aos comentários.';
+$lang['Invalid email adress.'] = 'Endereço de correio electrónico inválido.';
+$lang['Item'] = 'Item';
+$lang['Manage my subscriptions to comments'] = 'Gerir as minhas subscrições de comentários';
+$lang['Nothing to validate.'] = 'Nada para validar.';
+$lang['Notify me of followup comments'] = 'Notifique-me de futuros comentários';
+$lang['Only unsubscribe notifications for comments from:'] = 'Apenas cancelar subscrição de comentários de:';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Por favor consulte a sua caixa de correio electrónico para confirmar a sua subscrição.';
+$lang['Return to item page'] = 'Regressar à página do item';
+$lang['Subscribe without commenting'] = 'Subscrever sem comentar';
+$lang['Subscribe'] = 'Subscrever';
+$lang['Subscriptions of'] = 'Subscrições de';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Cancelou a subscrição para receber notificações por correio electrónico.';
+$lang['Unsubscribe from all email notifications'] = 'Cancelar todas as notificações por correio electrónico';
+$lang['Unsubscribe from email notification'] = 'Cancelar subscrição de notificação por correio electrónico';
+$lang['Unsubscribe'] = 'Cancelar subscrição';
+$lang['You are currently subscribed to comments.'] = 'Está subscrito aos comentários.';
+$lang['You are not subscribed to any comment.'] = 'Não está subscrito a nenhum comentário.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Foi adicionado à lista de subscritores deste álbum.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Foi adicionado à lista de subscritores desta foto.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'A sua subscrição foi validada, obrigado.';
+?>
