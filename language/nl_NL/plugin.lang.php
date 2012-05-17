3d4fbfb1806b002aa2b70814e715d34701d9acd2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-17 13:29:36 +0000

    [subscribe_to_comments] Update nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@15031 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Date'] = 'datum';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Ongeldig emailadres, je bent niet ingeschreven op commentaar.';
+$lang['Invalid email adress.'] = 'Ongeldig emailadres.';
+$lang['Item'] = 'item';
+$lang['Manage my subscriptions to comments'] = 'Beheer mijn inschrijvingen voor commentaar';
+$lang['Nothing to validate.'] = 'Niets om goed te keuren.';
+$lang['Notify me of followup comments'] = 'Notificeer mij over opvolgend commentaar';
+$lang['Only unsubscribe notifications for comments from:'] = 'Schrijf alleen uit van commentaar van:';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Bekijk je inbox om je inschrijving te bevestigen.';
+$lang['Return to item page'] = 'Keer terug naar de itempagina';
+$lang['Subscribe without commenting'] = 'Inschrijven zonder commentaar te schrijven';
+$lang['Subscribe'] = 'Inschrijven';
+$lang['Subscriptions of'] = 'Inschrijvingen van';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Je emailadres is uitgeschreven van het ontvangen van notificaties.';
+$lang['Unsubscribe from all email notifications'] = 'Schrijf uit van alle emailnotificaties';
+$lang['Unsubscribe from email notification'] = 'Schrijf uit van emailnotificatie';
+$lang['Unsubscribe'] = 'Uitschrijven';
+$lang['You are currently subscribed to comments.'] = 'Je bent nu ingeschreven op het commentaar.';
+$lang['You are not subscribed to any comment.'] = 'Je bent niet ingeschreven voor enig commentaar.';
+$lang['You have been added to the list of subscribers for this album.'] = 'Je bent toegevoegd aan de lijst van mensen die zich op dit album hebben ingeschreven.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Je bent toegevoegd aan de lijst van mensen die zich op deze afbeelding hebben ingescreven.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Je inschrijving is goedgekeurd, dank je.';
+?>
