aeae7ef77e6030d8d82340ed6e52dfafd8a498ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-28 12:20:06 +0000

    [subscribe_to_comments] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@15376 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Date'] = 'Data';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Adreça de correu electrònic no vàlida. No està inscrit als comentaris.';
+$lang['Invalid email adress.'] = 'Adreça de correu electrònic no vàlida';
+$lang['Item'] = 'Objecte';
+$lang['Manage my subscriptions to comments'] = 'Gestionar les meves inscripcions als comentaris';
+$lang['Nothing to validate.'] = 'No hi ha res per validar';
+$lang['Notify me of followup comments'] = 'Notificar-me en cas d\'haver-hi nous comentaris';
+$lang['Only unsubscribe notifications for comments from:'] = 'Només donar de baixa les notificacions per comentaris de:';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Revisi el seu correu electrònic per confirmar la seva inscripció.';
+$lang['Return to item page'] = 'Tornar a la pàgina anterior';
+$lang['Subscribe without commenting'] = 'Instriure\'s sense comentar';
+$lang['Subscribe'] = 'Instriure\'s';
+$lang['Subscriptions of'] = 'Incripcions de';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'La seva adreça de correu electrònic ha estat donat de baixa correctament. No rebrà més notificacions.';
+$lang['Unsubscribe from all email notifications'] = 'Donar-se de baixa de totes les notificacions per correu electrònic';
+$lang['Unsubscribe from email notification'] = 'Donar-se de baixa de les notificacions per correu electrònic';
+$lang['Unsubscribe'] = 'Donar de baixa';
+$lang['You are currently subscribed to comments.'] = 'Actualment ja està inscrit als comentaris.';
+$lang['You are not subscribed to any comment.'] = 'No està inscrit a cap comentari';
+$lang['You have been added to the list of subscribers for this album.'] = 'Se l\'ha afegit a la llista d\'usuaris inscrits per a aquest àlbum.';
+$lang['You have been added to the list of subscribers for this picture.'] = 'Se l\'ha afegit a la llista d\'usuaris inscrits per a aquesta imatge.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'La seva inscripció ha estat validada. Gràcies.';
+?>
