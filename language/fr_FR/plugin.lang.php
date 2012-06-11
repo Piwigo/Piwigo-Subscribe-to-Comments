5d7682ab8dd27c85bd549ebca11762af230eaf8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-11 20:27:08 +0000

    [subscribe_to_comments] Update fr_FR, thanks to : mistic100
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@15644 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -1,16 +1,47 @@
 <?php
 
 $lang['Please check your email inbox to confirm your subscription.'] = 'Veillez consulter votre boite mail pour confirmer votre inscription.';
-$lang['Invalid email adress, your are not subscribed to comments.'] = 'Addresse email invalide, vous n\'avez pas été inscrit.';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Adresse email invalide, vous n\'êtes pas inscrit.';
 $lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Inscription aux commentaires annulée avec succès.';
 $lang['Unsubscribe'] = 'Se désinscrire';
 $lang['Subscribe'] = 'S\'inscrire';
 $lang['Notify me of followup comments'] = 'Me notifier des nouveaux commentaires';
 $lang['Your subscribtion has been validated, thanks you.'] = 'Votre inscription a été validée, merci.';
-$lang['Subscriptions of'] = 'Abonnements de';
+$lang['Subscriptions of'] = 'Suivis de';
 $lang['Unsubscribe from all email notifications'] = 'Ne plus recevoir aucune notification';
 $lang['You are not subscribed to any comment.'] = 'Vous n\'êtes inscrit à aucun commentaire.';
 $lang['Return to item page'] = 'Retourner à la page de l\'élément';
 $lang['New comment on'] = 'Nouveau commentaire sur';
 
+$lang['%s has subscribed to comments on'] = '%s s\'est inscrit aux commentaires de';
+$lang['Allow users to subscribe to global notifications'] = 'Autoriser les utilisateurs à s\'inscrire de façon globale';
+$lang['Already validated.'] = 'Déjà validé.';
+$lang['Comments notifications'] = 'Suivis de commentaires';
+$lang['Confirm subscription'] = 'Confirmer l\'inscription';
+$lang['Confirm your subscribtion to comments'] = 'Confirmez votre inscription aux commentaires';
+$lang['Followed on'] = 'Inscrit depuis';
+$lang['Manage my subscribtions'] = 'Gérer mes suivis';
+$lang['New subscription on'] = 'Nouveau suivi sur';
+$lang['Not found.'] = 'Non trouvé.';
+$lang['Notify administrators when a user take a new subscription'] = 'Notifier les administrateurs des nouvelles inscriptions';
+$lang['Subject'] = 'Sujet';
+$lang['Subscribe to comments on'] = 'Inscription aux commentaires sur';
+$lang['Subscribe to mail notifications'] = 'Recevoir les nouveaux commentaires par mail';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Pour activer votre inscription, cliquez sur le bouton de confirmation. Si vous pensez que c\'est une erreur, merci d\'ignorer ce mail.';
+$lang['Validate'] = 'Valider';
+$lang['Want to edit your notifications options?'] = 'Je veux modifier mes options de suivi :';
+$lang['You are currently subscribed to comments on'] = 'Vous êtes actuellement inscrit aux commentaire sur';
+$lang['You have been added to the list of subscribers.'] = 'Vous avez été ajouté à la liste des inscrit.';
+$lang['You requested to subscribe by email to comments on'] = 'Vous avez demandé à recevoir par mail les nouveaux commentaires sur';
+$lang['all albums of the gallery'] = 'tous les albums du site';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'toutes les images de l\'album <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'toutes les images du site';
+$lang['all pictures of this album'] = 'toutes les images de cet album';
+$lang['comments on a picture'] = 'commentaires sur une image';
+$lang['comments on all pictures of an album'] = 'commentaires sur toutes les images d\'un album';
+$lang['comments on an album'] = 'commentaires sur un album';
+$lang['the album <a href="%s">%s</a>'] = 'l\'album <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'l\'image <a href="%s">%s</a>';
+$lang['this album'] = 'cet album';
+$lang['this picture'] = 'cette image';
 ?>
