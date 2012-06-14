7eb156d95083e175a339518a538291a8b81a6449
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-14 09:46:35 +0000

    [subscribe_to_comments] Update de_DE, thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@15663 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -12,4 +12,36 @@ $lang['Unsubscribe from all email notifications'] = 'Alle E-mailbenachrichtigung
 $lang['You are not subscribed to any comment.'] = 'Sie haben im Moment keine Benachrichtigungen abonniert.';
 $lang['Return to item page'] = 'Zurück zur Übersichtsseite der Einträge';
 
+$lang['%s has subscribed to comments on'] = '%s hat "Kommentieren" gewählt';
+$lang['Allow users to subscribe to global notifications'] = 'Benutzern erlauben, die globale Benachrichtigung zu abonnieren';
+$lang['Already validated.'] = 'Bereits validiert';
+$lang['Comments notifications'] = 'Benachrichtigungen für Kommentare';
+$lang['Confirm subscription'] = 'Abonnement bestätigen';
+$lang['Confirm your subscribtion to comments'] = 'Bestätigen Sie Ihr Abo für Kommentare';
+$lang['Followed on'] = 'Nachverfolgung an';
+$lang['Manage my subscriptions'] = 'Meine Abonnements verwalten';
+$lang['New comment on'] = 'Neue Kommentare an';
+$lang['New subscription on'] = 'Neues Abo an';
+$lang['Not found.'] = 'Nicht gefunden';
+$lang['Notify administrators when a user take a new subscription'] = 'Benachrichtigen Sie den Administrator, wenn ein Benutzer ein neues Abo anmeldet';
+$lang['Subject'] = 'Thema';
+$lang['Subscribe to comments on'] = 'Abonnieren Sie Kommentare';
+$lang['Subscribe to mail notifications'] = 'Abonnieren Sie Mail-Benachrichtigungen';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Zum aktivieren, klicken Sie auf die Bestätigungstaste. Wenn Sie glauben, dies sei ein Fehler, bitte einfach diese Meldung ignorieren';
+$lang['Validate'] = 'Bestätigen';
+$lang['Want to edit your notifications options?'] = 'Möchten Sie Ihre Mitteilungen Optionen bearbeiten?';
+$lang['You are currently subscribed to comments on'] = 'Sie abonnieren aktuell Kommentare';
+$lang['You have been added to the list of subscribers.'] = 'Sie sind der Liste für Abonnenten hinzugefügt';
+$lang['You requested to subscribe by email to comments on'] = 'Sie haben "Email für Kommentare" abonniert';
+$lang['all albums of the gallery'] = 'alle Alben der Galerie ';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'alle Bilder des Albums <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'alle Bilder der Galerie ';
+$lang['all pictures of this album'] = 'alle Bilder des Albums';
+$lang['comments on a picture'] = 'Kommentare für ein Bild';
+$lang['comments on all pictures of an album'] = 'Kommentare für alle Bilder eines Albums';
+$lang['comments on an album'] = 'Kommentare für ein Album';
+$lang['the album <a href="%s">%s</a>'] = 'das Album <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'das Bild <a href="%s">%s</a>';
+$lang['this album'] = 'dieses Album';
+$lang['this picture'] = 'diese Bild';
 ?>
