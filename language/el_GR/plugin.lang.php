b65674c4f749a55e9486280a89902c6249761aad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-15 22:10:40 +0000

    [subscribe_to_comments] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@15738 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s has subscribed to comments on'] = '%s έχει εγγραφεί σε σχόλια σχετικά με';
+$lang['Allow users to subscribe to global notifications'] = 'Επιτρέπει στους χρήστες να εγγραφούν σε συνολικές ειδοποιήσεις';
+$lang['Already validated.'] = 'Έχουν ήδη επικυρωθεί.';
+$lang['Comments notifications'] = 'Κοινοποίηση σχολίων';
+$lang['Confirm subscription'] = 'Επιβεβαίωση εγγραφής';
+$lang['Confirm your subscribtion to comments'] = 'Επιβεβαιώστε την εγγραφή σας στα σχόλια';
+$lang['Followed on'] = 'Ακολούθησε το';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Άκυρη η διεύθυνση σας e-mail, δεν εγγράφεστε σε σχόλια.';
+$lang['Manage my subscriptions'] = 'Διαχείριση των εγγραφών μου';
+$lang['New comment on'] = 'Νέο σχόλιο σχετικά με';
+$lang['New subscription on'] = 'Νέα εγγραφή στο';
+$lang['Not found.'] = 'Δεν βρέθηκε.';
+$lang['Notify administrators when a user take a new subscription'] = 'Να ειδοποιούνται οι διαχειριστές όταν ένας χρήστης λαμβάνει μια νέα συνδρομή';
+$lang['Notify me of followup comments'] = 'Να ειδοποιούμαι για παρακολούθηση σχολίων';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Παρακαλούμε ελέγξτε τα εισερχόμενα email σας για να επιβεβαιώσετε την εγγραφή σας.';
+$lang['Return to item page'] = 'Επιστροφή στη σελίδα του στοιχείου';
+$lang['Subject'] = 'Θέμα';
+$lang['Subscribe to comments on'] = 'Εγγραφείτε σε σχόλια σχετικά με';
+$lang['Subscribe to mail notifications'] = 'Εγγραφείτε για ειδοποιήσεις με email';
+$lang['Subscribe'] = 'Εγγραφή';
+$lang['Subscriptions of'] = 'Συνδρομές του';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Διαγραψατε με επιτυχία το e-mail σας από τις ανακοινώσεις ';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Για να ενεργοποιήσετε, κάντε κλικ στο κουμπί επιβεβαίωσης. Αν πιστεύετε ότι αυτό είναι ένα λάθος, παρακαλώ απλά αγνοήστε αυτό το μήνυμα.';
+$lang['Unsubscribe from all email notifications'] = 'Διαγραφή από όλες τις ειδοποιήσεις μέσω ηλεκτρονικού ταχυδρομείου';
+$lang['Unsubscribe'] = 'Διαγραφή';
+$lang['Validate'] = 'Επικύρωση';
+$lang['Want to edit your notifications options?'] = 'Θέλετε να επεξεργαστείτε τις επιλογές ειδοποιήσεών σας;';
+$lang['You are currently subscribed to comments on'] = 'Είστε εγγεγραμμένος αυτήν τη στιγμή σε σχόλια σχετικά με';
+$lang['You are not subscribed to any comment.'] = 'Δεν έχετε εγγραφεί σε οποιαδήποτε σχόλια.';
+$lang['You have been added to the list of subscribers.'] = 'Έχετε προστεθεί στον κατάλογο των συνδρομητών.';
+$lang['You requested to subscribe by email to comments on'] = 'Ζητήσατε να εγγραφείτε μέσω e-mail σε σχόλια σχετικά με';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Η εγγραφή σας έχει επικυρωθεί, σας ευχαριστούμαι.';
+$lang['all albums of the gallery'] = 'όλα τα λευκώματα της γκαλερί';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'όλες οι φωτογραφίες του λευκώματος <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'όλες οι φωτογραφίες της γκαλερί';
+$lang['all pictures of this album'] = 'Όλες οι εικόνες από αυτό το λεύκωμα';
+$lang['comments on a picture'] = 'σχόλια για μια εικόνα';
+$lang['comments on all pictures of an album'] = 'σχόλια σχετικά με όλες τις εικόνες ενός λευκώματος';
+$lang['comments on an album'] = 'σχόλια για ένα άλμπουμ';
+$lang['the album <a href="%s">%s</a>'] = 'το λεύκωμα <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'η εικάνα <a href="%s">%s</a>';
+$lang['this album'] = 'αυτό το λεύκωμα';
+$lang['this picture'] = 'αυτή η εικόνα';
+?>
