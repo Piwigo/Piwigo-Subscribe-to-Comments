decb445bbe3739f9f978264477272e7a18609a26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-30 12:51:51 +0000

    [subscribe_to_comments] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@16217 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s has subscribed to comments on'] = '%s yorumlara üye oldu:';
+$lang['Allow users to subscribe to global notifications'] = 'Kullanıcıların evrensel bildirimlere abone olmasına olanak tanır';
+$lang['Already validated.'] = 'Zaten doğrulandı.';
+$lang['Comments notifications'] = 'Yorum bildirimleri';
+$lang['Confirm subscription'] = 'Aboneliği onayla';
+$lang['Confirm your subscribtion to comments'] = 'Yorum aboneliklerinizi onaylayın';
+$lang['Followed on'] = 'Takibinde';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Geçersiz e-posta adresi, yorumlara abone olmadınız.';
+$lang['Manage my subscriptions'] = 'Aboneliklerimi yönet';
+$lang['New comment on'] = 'Yeni yorum:';
+$lang['New subscription on'] = 'Yeni abonelik:';
+$lang['Not found.'] = 'Bulunamadı.';
+$lang['Notify administrators when a user take a new subscription'] = 'Bir kullanıcı yeni bir abonelik aldığında yöneticileri bilgilendir';
+$lang['Notify me of followup comments'] = 'Takip eden yorumlarda beni bilgilendir';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Aboneliğinizi doğrulamak için lütfen e-posta adresinize ait gelen kutusunu kontrol ediniz.';
+$lang['Return to item page'] = 'İlgili sayfaya dön';
+$lang['Subject'] = 'Konu';
+$lang['Subscribe to comments on'] = 'Abone ol:';
+$lang['Subscribe to mail notifications'] = 'e-posta bildirimlerine abone ol';
+$lang['Subscribe'] = 'Abone ol';
+$lang['Subscriptions of'] = 'Abonelikler';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'e-Posta adresiniz bilgilendirme aboneliğinden çıkartılmıştır.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Etkinleştirmek için onayla butonuna tıklayınız. Eğer bu mesajın yanlışlıkla gönderilmiş olduğunu düşünüyorsanız lütfen dikkate almayınız.';
+$lang['Unsubscribe from all email notifications'] = 'Tüm e-Posta bildirimleri aboneliklerinden çık';
+$lang['Unsubscribe'] = 'Abonelikten çık';
+$lang['Validate'] = 'Doğrula';
+$lang['Want to edit your notifications options?'] = 'Bilgilendirme seçeneklerinizi düzenlemek ister misiniz ?';
+$lang['You are currently subscribed to comments on'] = 'Yorumlarına abone oldunuz:';
+$lang['You are not subscribed to any comment.'] = 'Herhangi bir yorum aboneliğiniz bulunmuyor.';
+$lang['You have been added to the list of subscribers.'] = 'Aboneler listesine eklendiniz.';
+$lang['You requested to subscribe by email to comments on'] = 'e-posta ile yorum aboneliği isteğinde bulundunuz:';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Aboneliğiniz doğrulandı, teşekkürler.';
+$lang['all albums of the gallery'] = 'galerinin tüm albümleri';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'albümdeki tüm resimler <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'galerideki tüm resimler';
+$lang['all pictures of this album'] = 'bu albümdeki tüm resimler';
+$lang['comments on a picture'] = 'bir resim hakkındaki yorumlar';
+$lang['comments on all pictures of an album'] = 'bir albümdeki tüm resimler hakkında yorumlar';
+$lang['comments on an album'] = 'bir albüm hakkında yorumlar';
+$lang['the album <a href="%s">%s</a>'] = 'albüm <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'resim <a href="%s">%s</a>';
+$lang['this album'] = 'bu albüm';
+$lang['this picture'] = 'bu resim';
+?>
