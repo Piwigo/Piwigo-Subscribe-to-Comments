456e5996760ea0a6a8c4c1cf15e15682642f94a2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-25 21:07:08 +0000

    [subscribe_to_comments] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@18241 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You requested to subscribe by email to comments on'] = 'Du har bedt om at blive tilmeldt mailkommentarer til';
+$lang['%s has subscribed to comments on'] = '%s er tilmeldt kommentarer til';
+$lang['New comment on'] = 'Ny kommentar til';
+$lang['New subscription on'] = 'Ny tilmelding til';
+$lang['Subscribe to comments on'] = 'Tilmeld til kommentarer til';
+$lang['the picture <a href="%s">%s</a>'] = 'billedet <a href="%s">%s</a>';
+$lang['this album'] = 'dette album';
+$lang['this picture'] = 'dette billede';
+$lang['Subscriptions of'] = 'Tilmeldinger til';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Din e-mail-adresse er frameldt modtagelse af notifikationer.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'For at aktiver skal du klikke på bekræft-knappen. Hvis du mener, at dette er en fejl, så vær venlig at ignorere denne mail.';
+$lang['Unsubscribe'] = 'Frameld';
+$lang['Unsubscribe from all email notifications'] = 'Frameld alle e-mail-notifikationer';
+$lang['Validate'] = 'Godkend';
+$lang['Want to edit your notifications options?'] = 'Ønsker du at redigere dine notifikationsvalgmuligheder?';
+$lang['You are currently subscribed to comments on'] = 'Du er i øjeblikket tilmeldt kommentarer til';
+$lang['You are not subscribed to any comment.'] = 'Du er ikke tilmeldt nogen kommentarer.';
+$lang['You have been added to the list of subscribers.'] = 'Du er blevet føjet til listen over tilmeldte.';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Tak, din tilmelding er godkendt.';
+$lang['all albums of the gallery'] = 'alle albummer i galleriet';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'alle billeder i albummet <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'alle billeder i galleriet';
+$lang['all pictures of this album'] = 'alle billeder i dette album';
+$lang['comments on a picture'] = 'kommentarer til et billede';
+$lang['comments on all pictures of an album'] = 'kommentarer til alle billeder i et album';
+$lang['comments on an album'] = 'kommentarer til et album';
+$lang['the album <a href="%s">%s</a>'] = 'albummet <a href="%s">%s</a>';
+$lang['Allow users to subscribe to global notifications'] = 'Giver brugere mulighed for at tilmelde sig globale notifikationer';
+$lang['Already validated.'] = 'Allerede godkendt.';
+$lang['Comments notifications'] = 'Kommentarnotifikationer';
+$lang['Confirm subscription'] = 'Bekræft tilmelding';
+$lang['Confirm your subscribtion to comments'] = 'Bekræft din tilmelding til kommentarer';
+$lang['Followed on'] = 'Fulgt den';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Ugyldig e-mail-adresse, du er ikke tilmeldt kommentarer.';
+$lang['Manage my subscriptions'] = 'Håndtér mine tilmeldinger';
+$lang['Not found.'] = 'Ikke fundet.';
+$lang['Notify administrators when a user take a new subscription'] = 'Notificer administrator når en bruger tilmelder sig noget nyt';
+$lang['Notify me of followup comments'] = 'Notificer mig om opfølgende kommentarer';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Kontroller din mailadresses indbakke for at bekræfte din tilmelding';
+$lang['Return to item page'] = 'Tilbage til element';
+$lang['Subject'] = 'Emne';
+$lang['Subscribe'] = 'Tilmeld';
+$lang['Subscribe to mail notifications'] = 'Tilmeld til mailnotifikationer';
+?>
