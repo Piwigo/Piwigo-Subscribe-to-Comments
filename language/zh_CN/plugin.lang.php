0a1c74a6e9b85a6bb773b9006a569da13c7616a0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-02 07:32:24 +0000

    [subscribe_to_comments] Update zh_CN, thanks to : jimhu
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@19255 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Allow users to subscribe to global notifications'] = '允许用户订阅全局通知';
+$lang['Already validated.'] = '已验证。';
+$lang['Comments notifications'] = '评论通知';
+$lang['Confirm subscription'] = '确认订阅';
+$lang['Confirm your subscribtion to comments'] = '确认订阅评论';
+$lang['Followed on'] = '跟随';
+$lang['Invalid email adress, your are not subscribed to comments.'] = '无效的 email 地址，您无法订阅评论。';
+$lang['Manage my subscriptions'] = '管理我的订阅';
+$lang['New comment on'] = '新的评论';
+$lang['New subscription on'] = '新的订阅';
+$lang['Not found.'] = '无法获取。';
+$lang['Notify administrators when a user take a new subscription'] = '当用户有新订阅时，通知管理员';
+$lang['Notify me of followup comments'] = '当有后续评论时，通知我';
+$lang['Please check your email inbox to confirm your subscription.'] = '请检查您的 email 收件箱来确认您的订阅。';
+$lang['Return to item page'] = '返回项目页面';
+$lang['Subject'] = '标题';
+$lang['Subscribe'] = '订阅';
+$lang['Subscribe to comments on'] = '订阅评论';
+$lang['Subscribe to mail notifications'] = '订阅 email 通知';
+$lang['Subscriptions of'] = '描述';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = '成功取消订阅接受通知。';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = '要激活，请单击确认按钮。如果您认为这是一个错误，请忽略这条消息。';
+$lang['Unsubscribe'] = '取消订阅';
+$lang['Unsubscribe from all email notifications'] = '取消所有 email 订阅通知';
+$lang['Validate'] = '有效';
+$lang['Want to edit your notifications options?'] = '希望变更您的通知选项？';
+$lang['You are currently subscribed to comments on'] = '您已经订阅了';
+$lang['You are not subscribed to any comment.'] = '您还没有订阅任何评论通知';
+$lang['You have been added to the list of subscribers.'] = '您已经被添加到订阅列表中。';
+$lang['You requested to subscribe by email to comments on'] = '您希望通过 email 订阅评论';
+$lang['Your subscribtion has been validated, thanks you.'] = '您的订阅已经生效，谢谢。';
+$lang['all albums of the gallery'] = '画廊中的所有相册';
+$lang['all pictures of the album <a href="%s">%s</a>'] = '相册 <a href="%s">%s</a> 中的所有相片';
+$lang['all pictures of the gallery'] = '画廊中的所有相片';
+$lang['all pictures of this album'] = '本相册中的所有相片';
+$lang['comments on a picture'] = '相片评论';
+$lang['comments on all pictures of an album'] = '相册中所有相片的评论';
+$lang['comments on an album'] = '相册的评论';
+$lang['the album <a href="%s">%s</a>'] = '相册 <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = '相片 <a href="%s">%s</a>';
+$lang['this album'] = '该相册';
+$lang['this picture'] = '该相片';
+$lang['%s has subscribed to comments on'] = '%s 订阅了评论';
+?>
