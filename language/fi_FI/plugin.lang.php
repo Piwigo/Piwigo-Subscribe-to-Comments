f88e83fd7697168ade440037637d27e76cb551ff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-03 18:15:23 +0000

    [subscribe_to_comments] Add fi_FI, thanks to : Juhi24
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@19760 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unsubscribe from all email notifications'] = 'Poista tilaukset kaikista sähköposti-ilmoituksista';
+$lang['all pictures of this album'] = 'kaikkiin kuviin tässä albumissa';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'kaikkiin kuviin albumissa <a href="%s">%s</a>';
+$lang['all albums of the gallery'] = 'kaikkiin gallerian ablumeihin';
+$lang['You are currently subscribed to comments on'] = 'Olet tilannut ilmoitukset kommenteista';
+$lang['all pictures of the gallery'] = 'kaikkiin gallerian kuviin';
+$lang['Your subscribtion has been validated, thanks you.'] = 'Kiitos, tilauksesi on vahvistettu.';
+$lang['Want to edit your notifications options?'] = 'Haluatko muuttaa ilmoitusasetuksiasi?';
+$lang['Return to item page'] = 'Palaa kohteen sivulle';
+$lang['Notify me of followup comments'] = 'Ilmoita minulle uusista kommenteista';
+$lang['this picture'] = 'tämä kuva';
+$lang['this album'] = 'tämä albumi';
+$lang['Unsubscribe'] = 'Poista tilaus';
+$lang['Subject'] = 'Aihe';
+$lang['Not found.'] = 'Ei löydy.';
+$lang['New subscription on'] = 'Uusi tilaus kohteeseen';
+$lang['New comment on'] = 'Uusi kommentti kohteeseen';
+$lang['Manage my subscriptions'] = 'Hallitse tilauksia';
+$lang['Confirm your subscribtion to comments'] = 'Vahvista kommenttien tilaus';
+$lang['Confirm subscription'] = 'Vahvista tilaus';
+$lang['%s has subscribed to comments on'] = '%s on tilannut kommentit kohteeseen';
+?>
