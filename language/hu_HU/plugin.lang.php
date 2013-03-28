d5e57384747154414809e9c47f241ff39bb1147b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-28 20:39:07 +0000

    [subscribe_to_comments] Add hu_HU, thanks to : miky
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@21885 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,73 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Followed on'] = 'Követve';
+$lang['Confirm subscription'] = 'Feliratkozás jóváhagyása';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Az aktiváláshoz kattints a jóváhagyás gombra. Ha úgy gondolod valamilyen hiba történt, akkor egyszerűen hagyd figyelmen kívül ezt az üzenetet.';
+$lang['%s has subscribed to comments on %s.'] = '%s feliratkozott a hozzászólásokra ehhez %s.';
+$lang['&#9733; : registered users'] = '&#9733; : regisztrált felhasználók';
+$lang['All album photos'] = 'Az album összes képe';
+$lang['Allow users to subscribe to global notifications'] = 'Engedélyezés, hogy a felhasználók feliratkozzanak globális értesítésekre';
+$lang['Already validated.'] = 'Már érvényesítve lett.';
+$lang['New subscription on %s'] = 'Új feliratkozás ehhez %s';
+$lang['No result'] = 'Nincs találat';
+$lang['Notify administrators when a user take a new subscription'] = 'Adminisztrátorok értesítése, amikor egy felhasználó feliratkozik valamire';
+$lang['Notify me of followup comments'] = 'Értesítsen a további hozzászólásokról';
+$lang['Please check your email inbox to confirm your subscription.'] = 'Kérjük ellenőrizd a postafiókod, hogy érvényesítsd a feliratkozásod';
+$lang['Subscriptions of %s'] = '%s feliratkozásai';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Sikeresen leiratkoztál az értesítések fogadásáról.';
+$lang['Return to item page'] = 'Visszatérés a elem oldalához';
+$lang['Stop receiving notifications'] = 'Értesítések fogadásának tiltása';
+$lang['Subject'] = 'Tárgy';
+$lang['Subscribe'] = 'Feliratkozás';
+$lang['Subscribe to comments on %s'] = 'Feliratkozás a hozzászólásokra ehhez %s';
+$lang['Subscribe to mail notifications'] = 'Feliratkozás email értesítésekre';
+$lang['Validate'] = 'Érvényesítés';
+$lang['You have been added to the list of subscribers.'] = 'Hozzál lettél adva a feliratkozottak listájához.';
+$lang['You requested to subscribe by email to comments on %s'] = 'Kérvényezted, hogy feliratkozz az emailben érkező hozzászólásokra ehhez: %s';
+$lang['Your subscribtion has been validated, thanks you.'] = 'A feliratkozásod érvényesítve lett, köszönjük.';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'az album összes képe <a href="%s">%s</a>';
+$lang['the album <a href="%s">%s</a>'] = 'az album <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'a kép <a href="%s">%s</a>';
+$lang['this album'] = 'ez az album';
+$lang['this picture'] = 'ez a kép';
+$lang['Unsubscribe'] = 'Leiratkozás';
+$lang['Unsubscribe from all email notifications'] = 'Leiratkozás az összes email értesítőről';
+$lang['Want to edit your notifications options?'] = 'Szerkeszteni akarod az értesítési beállításokat?';
+$lang['You are currently subscribed to comments on %s.'] = 'Jelenleg fel vagy iratkozva a hozzászólásokra ehhez %s.';
+$lang['You are not subscribed to any comment.'] = 'Nem vagy feliratkozva hozzászólásokra.';
+$lang['all albums of the gallery'] = 'a galériának az összes albumba';
+$lang['all pictures of the gallery'] = 'a galériának az összes képe';
+$lang['all pictures of this album'] = 'ennek az albumnak az összes képe';
+$lang['comments on a picture'] = 'hozzászólások egy képen';
+$lang['comments on all pictures of an album'] = 'hozzászólások egy album összes képén';
+$lang['comments on an album'] = 'hozzászólások egy albumon';
+$lang['Comments notifications'] = 'Hozzászólás értesítések';
+$lang['Confirm your subscribtion to comments'] = 'Erősítsd meg a feliratkozásod a hozzászólásokra';
+$lang['First subscription'] = 'Első feliratkozás';
+$lang['Global subscriptions'] = 'Globális feliratkozások';
+$lang['Invalid email adress, your are not subscribed to comments.'] = 'Helytelen email cím, ezért nem iratkoztál fel a hozzászólásokra.';
+$lang['Last subscription'] = 'Utolsó feliratkozás';
+$lang['Manage my subscriptions'] = 'Feliratkozásaim kezelése';
+$lang['New comment on %s'] = 'Új hozzászólás a képhez: %s ';
+$lang['Not found.'] = 'Nem található.';
+?>
