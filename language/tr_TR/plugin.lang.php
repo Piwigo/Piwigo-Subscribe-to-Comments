7344d4575154a049737e5c7c5f4da0b977e4b831
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-24 06:23:18 +0000

    [subscribe_to_comments] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@26148 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -20,7 +20,6 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['%s has subscribed to comments on'] = '%s yorumlara üye oldu:';
 $lang['Allow users to subscribe to global notifications'] = 'Kullanıcıların evrensel bildirimlere abone olmasına olanak tanır';
 $lang['Already validated.'] = 'Zaten doğrulandı.';
 $lang['Comments notifications'] = 'Yorum bildirimleri';
@@ -29,28 +28,22 @@ $lang['Confirm your subscription to comments'] = 'Yorum aboneliklerinizi onaylay
 $lang['Followed on'] = 'Takibinde';
 $lang['Invalid email address, your are not subscribed to comments.'] = 'Geçersiz e-posta adresi, yorumlara abone olmadınız.';
 $lang['Manage my subscriptions'] = 'Aboneliklerimi yönet';
-$lang['New comment on'] = 'Yeni yorum:';
-$lang['New subscription on'] = 'Yeni abonelik:';
 $lang['Not found.'] = 'Bulunamadı.';
 $lang['Notify administrators when a user take a new subscription'] = 'Bir kullanıcı yeni bir abonelik aldığında yöneticileri bilgilendir';
 $lang['Notify me of followup comments'] = 'Takip eden yorumlarda beni bilgilendir';
 $lang['Please check your email in-box to confirm your subscription.'] = 'Aboneliğinizi doğrulamak için lütfen e-posta adresinize ait gelen kutusunu kontrol ediniz.';
 $lang['Return to item page'] = 'İlgili sayfaya dön';
 $lang['Subject'] = 'Konu';
-$lang['Subscribe to comments on'] = 'Abone ol:';
 $lang['Subscribe to mail notifications'] = 'e-posta bildirimlerine abone ol';
 $lang['Subscribe'] = 'Abone ol';
-$lang['Subscriptions of'] = 'Abonelikler';
 $lang['Successfully unsubscribed your email address from receiving notifications.'] = 'e-Posta adresiniz bilgilendirme aboneliğinden çıkartılmıştır.';
 $lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Etkinleştirmek için onayla butonuna tıklayınız. Eğer bu mesajın yanlışlıkla gönderilmiş olduğunu düşünüyorsanız lütfen dikkate almayınız.';
 $lang['Unsubscribe from all email notifications'] = 'Tüm e-Posta bildirimleri aboneliklerinden çık';
 $lang['Unsubscribe'] = 'Abonelikten çık';
 $lang['Validate'] = 'Doğrula';
 $lang['Want to edit your notifications options?'] = 'Bilgilendirme seçeneklerinizi düzenlemek ister misiniz ?';
-$lang['You are currently subscribed to comments on'] = 'Yorumlarına abone oldunuz:';
 $lang['You are not subscribed to any comment.'] = 'Herhangi bir yorum aboneliğiniz bulunmuyor.';
 $lang['You have been added to the list of subscribers.'] = 'Aboneler listesine eklendiniz.';
-$lang['You requested to subscribe by email to comments on'] = 'e-posta ile yorum aboneliği isteğinde bulundunuz:';
 $lang['Your subscription has been validated, thanks you.'] = 'Aboneliğiniz doğrulandı, teşekkürler.';
 $lang['all albums of the gallery'] = 'galerinin tüm albümleri';
 $lang['all pictures of the album <a href="%s">%s</a>'] = 'albümdeki tüm resimler <a href="%s">%s</a>';
@@ -66,7 +59,6 @@ $lang['this picture'] = 'bu resim';
 $lang['You requested to subscribe by email to comments on %s.'] = '%s \'deki yorumlara e-posta yoluyla abonelik isteği yaptınız.';
 $lang['You are currently subscribed to comments on %s.'] = '%s \'deki yorumlara abone oldunuz.';
 $lang['Subscriptions of %s'] = '%s \'deki abonelikler';
-$lang['Subscribe to comments on %s'] = '%s \'deki yorumlara abone ol';
 $lang['New comment on %s'] = '%s \'de yeni yorum';
 $lang['%s has subscribed to comments on %s.'] = '%s, %s \'deki yorumlara abone oldu.';
 $lang['Stop receiving notifications'] = 'Bilgilendirme alımını durdur';
@@ -77,4 +69,8 @@ $lang['Last subscription'] = 'Son abonelik';
 $lang['All album photos'] = 'Tüm albüm fotoğrafları';
 $lang['&#9733; : registered users'] = '&#9733; : kayıtlı kullanıcılar';
 $lang['No result'] = 'Sonuç yok';
+$lang['Subscribe without commenting'] = 'Yorumsuz olarak abone ol';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> yazdı <i>%s</i>';
+$lang['Apply action'] = 'Eylemi uygula';
+$lang['Choose an action'] = 'Bir eylem seçin';
 ?>
