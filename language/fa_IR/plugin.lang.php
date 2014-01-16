7c8d9d6a31ee990ea22c96b839d73df575ea824c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 18:18:48 +0000

    [subscribe_to_comments] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@26808 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Followed on'] = 'پیگیری میکنددر';
+$lang['Subject'] = 'موضوع';
+$lang['All album photos'] = 'تمام عکس های البوم ';
+$lang['Allow users to subscribe to global notifications'] = '
+به کاربران اجازه اشتراک در اطلاعیه های عمومی می دهد';
+$lang['Already validated.'] = 'در حال حاضر تایید شده است.';
+$lang['Apply action'] = 'پذیرفتن ایکن';
+$lang['Choose an action'] = 'انتخاب فعالیت';
+$lang['%s has subscribed to comments on %s.'] = '
+ %sبه نظرات در اشتراک است%s. ';
+$lang['&#9733; : registered users'] = '
+کاربر ثبت شده&#9733; :';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<i>%s</i>نوشت در<b>%s</b>';
+$lang['Subscribe without commenting'] = 'اشتراک بدون اظهار نظر کردن';
+?>
