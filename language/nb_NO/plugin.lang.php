934724852657d8d3e98bdbd88355eac9c561844c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-24 17:08:05 +0000

    [subscribe_to_comments] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Subscribe_to_comments@30289 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,75 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums of the gallery'] = 'alle album i galleriet';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'alle bilder i albumet <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'alle bilder i galleriet';
+$lang['all pictures of this album'] = 'alle bilder i dette albumet';
+$lang['comments on a picture'] = 'kommentarer til et bilde';
+$lang['comments on all pictures of an album'] = 'kommentarer på alle bilder i en album';
+$lang['comments on an album'] = 'kommentarer på en album';
+$lang['the album <a href="%s">%s</a>'] = 'albumet <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'bilde <a href="%s">%s</a>';
+$lang['this album'] = 'dette albumet';
+$lang['this picture'] = 'dette bilde';
+$lang['Your subscription has been validated, thanks you.'] = 'Abonnementet har blitt godkjent, takk skal du ha.';
+$lang['Already validated.'] = 'Allerede sjekket gyldigheten.';
+$lang['You requested to subscribe by email to comments on %s.'] = 'Du har bedt om å abonnere på kommentarer via e-post %s ..';
+$lang['Not found.'] = 'Ikke funnet.';
+$lang['Notify administrators when a user take a new subscription'] = 'Varsle administratorer når en bruker tegner et nytt abonnement';
+$lang['Notify me of followup comments'] = 'Varsle meg om oppfølgings kommentarer';
+$lang['Please check your email in-box to confirm your subscription.'] = 'Vennligst sjekk din e-posten for å bekrefte ditt abonnement.';
+$lang['Return to item page'] = 'Gå tilbake til punkt siden';
+$lang['Stop receiving notifications'] = 'Slutte å motta varslinger';
+$lang['Subject'] = 'Emne';
+$lang['Subscribe'] = 'Abonner';
+$lang['Subscribe to mail notifications'] = 'Abonner på e-post varslinger';
+$lang['Subscribe without commenting'] = 'Abonner uten å kommentere';
+$lang['Subscriptions of %s'] = 'Abonnement av %s';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Abonnementet for e-post varsling avsluttet.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'For å aktivere, klikk på bekreft knappen. Hvis du mener dette er en feil, kan du bare ignorere denne meldingen.';
+$lang['Unsubscribe'] = 'Avslutt abonnementet';
+$lang['Unsubscribe from all email notifications'] = 'Avslutte abonnementet for alle e-postvarsler';
+$lang['Validate'] = 'Sjekk gyldigheten';
+$lang['Want to edit your notifications options?'] = 'Ønsker du å redigere dine varslings alternativer?';
+$lang['You are currently subscribed to comments on %s.'] = 'Du abonnerer for tiden på kommentarer %s.';
+$lang['You are not subscribed to any comment.'] = 'Du abonnerer ikke på noen kommentar.';
+$lang['You have been added to the list of subscribers.'] = 'Du har blitt lagt til listen over abonnenter.';
+$lang['%s has subscribed to comments on %s.'] = '%s har abonert på kommentarer %s.';
+$lang['&#9733; : registered users'] = '&#9733; : registrerte brukere';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> skrev på <i>%s</i>';
+$lang['All album photos'] = 'Alle album bilder';
+$lang['Allow users to subscribe to global notifications'] = 'Tillat brukere å abonnere på globale varslinger';
+$lang['Apply action'] = 'Legg til handling';
+$lang['Choose an action'] = 'Velg en handling';
+$lang['Comments notifications'] = 'Kommentar varslinger';
+$lang['Confirm subscription'] = 'Bekreft abonnement';
+$lang['Confirm your subscription to comments'] = 'Bekreft ditt abonement på kommentarer';
+$lang['First subscription'] = 'Første abonement';
+$lang['Followed on'] = 'Fulgte på';
+$lang['Global subscriptions'] = 'Global abonement';
+$lang['Invalid email address, your are not subscribed to comments.'] = 'Ugyldig e-postadresse, du abonnerer ikke på kommentarer.';
+$lang['Last subscription'] = 'Siste abonement';
+$lang['Manage my subscriptions'] = 'Behandle mitt abonement';
+$lang['New comment on %s'] = 'Nye kommentarer på %s';
+$lang['New subscription on %s'] = 'Nye abonementer på %s';
+$lang['No result'] = 'Ingen resultat';
