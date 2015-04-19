ca1f1caf3480d8958e06e1285eacc234d95523ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-19 14:20:02 +0200

    Add sv_SE, thanks to wahlis

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,75 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Comments notifications'] = 'Meddelande om kommentarer';
+$lang['Return to item page'] = 'Åter till sidan';
+$lang['Subscriptions of %s'] = 'Prenumeration på %s';
+$lang['You have been added to the list of subscribers.'] = 'Du är tillagd i listan på prenumeranter.';
+$lang['You requested to subscribe by email to comments on %s.'] = 'Du beställde en e-post prenumeration på kommenterar till %s.';
+$lang['Your subscription has been validated, thanks you.'] = 'Din prenumeration är nu validerad. Tack.';
+$lang['all albums of the gallery'] = 'alla album i galleriet.';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'alla bilder i albumet <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'alla bilder i galleriet';
+$lang['all pictures of this album'] = 'alla bilder i detta album';
+$lang['comments on a picture'] = 'kommentarer till en bild ';
+$lang['comments on all pictures of an album'] = 'kommentarer till alla bilder i ett album';
+$lang['comments on an album'] = 'kommentarer till ett album';
+$lang['the album <a href="%s">%s</a>'] = 'albumet <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'bilden <a href="%s">%s</a>';
+$lang['this album'] = 'detta album';
+$lang['this picture'] = 'denna bild';
+$lang['Not found.'] = 'Hittades inte.';
+$lang['Notify administrators when a user take a new subscription'] = 'Meddela administratörerna när en användare skapar en prenumeration.';
+$lang['Notify me of followup comments'] = 'Meddela mig om vidare kommentarer.';
+$lang['Please check your email in-box to confirm your subscription.'] = 'Kolla din e-post för att bekräfta prenumerationen.';
+$lang['Stop receiving notifications'] = 'Sluta ta emot notifieringar';
+$lang['Subject'] = 'Ämne';
+$lang['Subscribe'] = 'Prenumerera';
+$lang['Subscribe to mail notifications'] = 'Prenumerera på e-postnotifieringar';
+$lang['Subscribe without commenting'] = 'Prenumerera utan kommentar';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Notifieringar skickas inte längre till e-postadressen.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Klicka bekräfta för att aktivera. Om detta inte är vad du vill göra så ignorera detta meddelande.';
+$lang['Unsubscribe'] = 'Avbryt prenumeration';
+$lang['Unsubscribe from all email notifications'] = 'Avanmäl från alla e-postnotifieringar';
+$lang['Validate'] = 'Validera';
+$lang['Want to edit your notifications options?'] = 'Vill du redigera dina notifieringsinställningar?';
+$lang['You are currently subscribed to comments on %s.'] = 'Du prenumererar för närvarande på kommentarer på %s';
+$lang['You are not subscribed to any comment.'] = 'Du är inte prenumerant på några kommentarer.';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> skrev den <i>%s</i>';
+$lang['All album photos'] = 'Alla bilder i albumet';
+$lang['Allow users to subscribe to global notifications'] = 'Tillåt att användarna prenumererar på globala notifieringar';
+$lang['Already validated.'] = 'Redan validerad.';
+$lang['Apply action'] = 'Tillämpa';
+$lang['Choose an action'] = 'Välj aktivitet';
+$lang['Confirm subscription'] = 'Bekräfta prenumeration';
+$lang['Confirm your subscription to comments'] = 'Bekräfta din prenumeration på kommentarer';
+$lang['First subscription'] = 'Första prenumeration';
+$lang['Followed on'] = 'Följd på';
+$lang['Global subscriptions'] = 'Globala prenumerationer';
+$lang['Invalid email address, your are not subscribed to comments.'] = 'Ogiltig e-postadress, du är inte anmäld som prenumerant på kommentarer.';
+$lang['Last subscription'] = 'Senaste prenumeration';
+$lang['Manage my subscriptions'] = 'Hantera prenumerationer';
+$lang['New comment on %s'] = 'Ny kommentar den %s';
+$lang['New subscription on %s'] = 'Ny prenumeration den %s';
+$lang['No result'] = 'Inget resultat';
+$lang['%s has subscribed to comments on %s.'] = '%s prenumererar på kommentarer på %s.';
+$lang['&#9733; : registered users'] = '&#9733; registrerade användare';
