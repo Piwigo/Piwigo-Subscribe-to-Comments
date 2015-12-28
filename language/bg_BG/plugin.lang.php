65822d48305f98cca2f5b817c65846a88cc8a3aa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-12-28 19:09:59 +0100

    Add bg_BG, thanks to turnokopa

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Subscriptions of %s'] = 'Абонирания за %s';
+$lang['Subject'] = 'Субект';
+$lang['Not found.'] = 'Не е намерено.';
+$lang['Manage my subscriptions'] = 'Управление на моя абонамент';
+$lang['Last subscription'] = 'Последен абонамент';
+$lang['all albums of the gallery'] = 'всички албуми в тази галерия';
+$lang['all pictures of the gallery'] = 'всички снимки в тази галерия';
+$lang['all pictures of this album'] = 'всички снимки от този албум';
+$lang['Unsubscribe'] = 'Отписване';
+$lang['Validate'] = 'Потвърждаване';
+$lang['comments on an album'] = 'коментари към албуми';
+$lang['comments on all pictures of an album'] = 'коментари към всички снимки на всички албуми ';
+$lang['comments on a picture'] = 'коментири към снимката';
+$lang['this picture'] = 'тази снимка';
+$lang['this album'] = 'този албум';
+$lang['Subscribe without commenting'] = 'Абониране без коментиране';
+$lang['Subscribe to mail notifications'] = 'Абониране за съобщения по имейл';
+$lang['Subscribe'] = 'Абонирай';
+$lang['First subscription'] = 'Първи коментар';
+$lang['Confirm your subscription to comments'] = 'Потвърдете вашият абонамент за коментари';
+$lang['Comments notifications'] = 'Уведомяване за коментари';
+$lang['Confirm subscription'] = 'Потвърди абонамент';
+$lang['Allow users to subscribe to global notifications'] = 'Позволи абониране на потребители за глобални съобщения';
+$lang['All album photos'] = 'Всички снимки в албуми';
+$lang['Already validated.'] = 'Вече е потвърдено.';
+$lang['Apply action'] = 'Потвърди действие';
+$lang['Choose an action'] = 'Избор на действие';
+$lang['No result'] = 'Няма резулнат';
