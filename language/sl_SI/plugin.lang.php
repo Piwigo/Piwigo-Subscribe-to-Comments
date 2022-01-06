bfee80ccfc523b84a6672e53fb27b5030ff7a8d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-06 20:22:33 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['all pictures of the gallery'] = 'vse slike iz galerije';
+$lang['all pictures of this album'] = 'vse slike tega albuma';
+$lang['comments on a picture'] = 'komentarji na sliko';
+$lang['comments on all pictures of an album'] = 'komentarji na vse slike albuma';
+$lang['comments on an album'] = 'komentarji na album';
+$lang['the album <a href="%s">%s</a>'] = 'album <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'slika <a href="%s">%s</a>';
+$lang['this album'] = 'ta album';
+$lang['this picture'] = 'ta slika';
+$lang['Unsubscribe'] = 'Odjavi se';
+$lang['Unsubscribe from all email notifications'] = 'Odjava od vseh e-poštnih obvestil';
+$lang['Validate'] = 'Potrdi';
+$lang['Want to edit your notifications options?'] = 'Želite urediti možnosti za obvestila?';
+$lang['You are currently subscribed to comments on %s.'] = 'Trenutno ste naročeni na komentarje na %s.';
+$lang['You are not subscribed to any comment.'] = 'Niste naročeni na noben komentar.';
+$lang['You have been added to the list of subscribers.'] = 'Dodani ste bili na seznam naročnikov.';
+$lang['You requested to subscribe by email to comments on %s.'] = 'Zahtevali ste, da se naročite po e-pošti na komentarje na %s.';
+$lang['Your subscription has been validated, thanks you.'] = 'Vaša naročnina je bila potrjena, hvala.';
+$lang['all albums of the gallery'] = 'vsi albumi galerije';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'vse slike albuma <a href="%s">%s</a>';
+$lang['Notify me of followup comments'] = 'Obvesti me o nadaljnjih komentarjih';
+$lang['Please check your email in-box to confirm your subscription.'] = 'Preverite svoj e-poštni predal, da potrdite svojo naročnino.';
+$lang['Return to item page'] = 'Nazaj na stran predmeta';
+$lang['Stop receiving notifications'] = 'Nehajte prejemati obvestila';
+$lang['Subject'] = 'Zadeva';
+$lang['Subscribe'] = 'Naročite se';
+$lang['Subscribe to mail notifications'] = 'Naročite se na obvestila po pošti';
+$lang['Subscribe without commenting'] = 'Naročite se brez komentarjev';
+$lang['Subscriptions of %s'] = 'Naročnine na %s';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Vaš e-poštni naslov je bil uspešno odjavljen od prejemanja obvestil.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Če želite aktivirati, kliknite gumb za potrditev. Če menite, da gre za napako, tega sporočila ne upoštevajte.';
+$lang['First subscription'] = 'Prva naročnina';
+$lang['Followed on'] = 'Sledi na';
+$lang['Global subscriptions'] = 'Globalne naročnine';
+$lang['Invalid email address, your are not subscribed to comments.'] = 'Neveljaven e-poštni naslov, niste naročeni na komentarje.';
+$lang['Last subscription'] = 'Zadnja naročnina';
+$lang['Manage my subscriptions'] = 'Upravljaj moje naročnine';
+$lang['New comment on %s'] = 'Nov komentar na %s';
+$lang['New subscription on %s'] = 'Nova naročnina na %s';
+$lang['No result'] = 'Brez rezultata';
+$lang['Not found.'] = 'Ni najdeno.';
+$lang['Notify administrators when a user take a new subscription'] = 'Obvesti skrbnike, ko uporabnik sprejme novo naročnino';
+$lang['%s has subscribed to comments on %s.'] = '%s se je naročil na komentarje na %s.';
+$lang['&#9733; : registered users'] = '&#9733; : registrirani uporabniki';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> napisal na <i>%s</i>';
+$lang['All album photos'] = 'Vse slike albuma';
+$lang['Allow users to subscribe to global notifications'] = 'Dovoli uporabnikom, da se naročijo na globalna obvestila';
+$lang['Already validated.'] = 'Že potrjeno.';
+$lang['Apply action'] = 'Uporabite dejanje';
+$lang['Choose an action'] = 'Izberite dejanje';
+$lang['Comments notifications'] = 'Obvestila o komentarjih';
+$lang['Confirm subscription'] = 'Potrdite naročnino';
+$lang['Confirm your subscription to comments'] = 'Potrdite svojo naročnino na komentarje';
