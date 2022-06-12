03c3c5555f934edb6bbe1f4151dabedb2b455049
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-12 06:32:37 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You are not subscribed to any comment.'] = 'Þú ert ekki áskrifandi að neinum umsögnum.';
+$lang['You have been added to the list of subscribers.'] = 'Þér hefur verið bætt við listann yfir áskrifendur.';
+$lang['You requested to subscribe by email to comments on %s.'] = 'Þú baðst um að gerast áskrifandi með tölvupósti að umsögnum á %s..';
+$lang['Your subscription has been validated, thanks you.'] = 'Áskriftin þín hefur verið staðfest, takk fyrir.';
+$lang['all albums of the gallery'] = 'öll albúm myndasafnsins';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'allar myndir albúmsins <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'allar myndir í myndasafninu';
+$lang['all pictures of this album'] = 'allar myndir í þessu albúmi';
+$lang['comments on a picture'] = 'umsagnir við mynd';
+$lang['comments on all pictures of an album'] = 'umsagnir við allar myndir í albúmi';
+$lang['comments on an album'] = 'umsagnir við albúm';
+$lang['the album <a href="%s">%s</a>'] = 'albúmið <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'myndin <a href="%s">%s</a>';
+$lang['this album'] = 'þetta albúm';
+$lang['this picture'] = 'þessi mynd';
+$lang['New subscription on %s'] = 'Ný áskrift á %s';
+$lang['No result'] = 'Engin niðurstaða';
+$lang['Not found.'] = 'Fannst ekki.';
+$lang['Notify administrators when a user take a new subscription'] = 'Látið stjórnendur vita þegar notandi fær sér nýja áskrift';
+$lang['Notify me of followup comments'] = 'Láttu mig vita af eftirfylgni umsagna';
+$lang['Please check your email in-box to confirm your subscription.'] = 'Vinsamlega athugaðu tölvupóstinn þinn til að staðfesta áskriftina þína.';
+$lang['Return to item page'] = 'Fara aftur á atriðasíðu';
+$lang['Stop receiving notifications'] = 'Hættu að fá tilkynningar';
+$lang['Subject'] = 'Efni';
+$lang['Subscribe'] = 'Gerast áskrifandi';
+$lang['Subscribe to mail notifications'] = 'Gerast áskrifandi að tölvupóst tilkynningum';
+$lang['Subscribe without commenting'] = 'Gerast áskrifandi án þess að skrifa umsagnir';
+$lang['Subscriptions of %s'] = 'Áskrift að %s';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'Það tókst að afskrá netfangið þitt frá því að fá tilkynningar.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'Til að virkja, smelltu á staðfestingarhnappinn. Ef þú telur að þetta sé villa skaltu bara hunsa þessi skilaboð.';
+$lang['Unsubscribe'] = 'Hætta áskrift';
+$lang['Unsubscribe from all email notifications'] = 'Hætta áskrift að öllum tölvupóst tilkynningum';
+$lang['Validate'] = 'Staðfesta';
+$lang['Want to edit your notifications options?'] = 'Viltu breyta þínum tilkynningavalkostum?';
+$lang['You are currently subscribed to comments on %s.'] = 'Þú ert núna áskrifandi að umsögnum á %s.';
+$lang['Choose an action'] = 'Veldu aðgerð';
+$lang['Comments notifications'] = 'Tilkynningar umsagna';
+$lang['Confirm subscription'] = 'Staðfestu áskrift';
+$lang['Confirm your subscription to comments'] = 'Staðfestu áskrift þína að umsögnum';
+$lang['First subscription'] = 'Fyrsta áskrift';
+$lang['Followed on'] = 'Fylgst með á';
+$lang['Global subscriptions'] = 'Almenn áskrift';
+$lang['Invalid email address, your are not subscribed to comments.'] = 'Ógilt netfang, þú ert ekki áskrifandi að umsögnum.';
+$lang['Last subscription'] = 'Síðasta áskrift';
+$lang['Manage my subscriptions'] = 'Stjórna mínum áskriftum';
+$lang['New comment on %s'] = 'Ný umsögn þann %s';
+$lang['%s has subscribed to comments on %s.'] = '%s hefur gerst áskrifandi að umsögnum við %s.';
+$lang['&#9733; : registered users'] = '&#9733; : nýskráðir notendir';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> skrifaði þann <i>%s</i>';
+$lang['All album photos'] = 'Allar albúm myndir';
+$lang['Allow users to subscribe to global notifications'] = 'Leyfa notendum að gerast áskrifendur að almennum tilkynningum';
+$lang['Already validated.'] = 'Þegar staðfest.';
+$lang['Apply action'] = 'Beita aðgerð';
