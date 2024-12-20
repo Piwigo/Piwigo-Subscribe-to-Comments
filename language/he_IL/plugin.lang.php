ffc9744cc9b3f3d3efa5f86bb735e95654e13423
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 17:56:46 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s has subscribed to comments on %s.'] = '%s נרשם לתגובות על %s.';
+$lang['&#9733; : registered users'] = '&#9733; : משתמשים רשומים';
+$lang['<b>%s</b> wrote on <i>%s</i>'] = '<b>%s</b> כתב על <i>%s</i>';
+$lang['All album photos'] = 'כל התמונות באלבום';
+$lang['Allow users to subscribe to global notifications'] = 'לאפשר למשתמשים להירשם להתראות גלובליות';
+$lang['Already validated.'] = 'כבר אומת.';
+$lang['Apply action'] = 'החל פעולה';
+$lang['Choose an action'] = 'בחר פעולה';
+$lang['Comments notifications'] = 'התראות תגובות';
+$lang['Confirm subscription'] = 'אשר הרשמה';
+$lang['Confirm your subscription to comments'] = 'אשר את הרישום שלך לתגובות';
+$lang['First subscription'] = 'הרשמה ראשונה';
+$lang['Followed on'] = 'נעקב על';
+$lang['Global subscriptions'] = 'הרשמות גלובליות';
+$lang['Invalid email address, your are not subscribed to comments.'] = 'כתובת דוא"ל לא חוקית, אינך רשום לתגובות.';
+$lang['Last subscription'] = 'הרשמה אחרונה';
+$lang['Manage my subscriptions'] = 'ניהול ההרשמות שלי';
+$lang['New comment on %s'] = 'תגובה חדשה על %s';
+$lang['New subscription on %s'] = 'הרשמה חדשה על %s';
+$lang['No result'] = 'אין תוצאה';
+$lang['Not found.'] = 'לא נמצא.';
+$lang['Notify administrators when a user take a new subscription'] = 'להודיע למנהלים כאשר משתמש עושה הרשמה חדשה';
+$lang['Notify me of followup comments'] = 'להודיע לי על תגובות נוספות';
+$lang['Please check your email in-box to confirm your subscription.'] = 'אנא בדוק את תיבת הדואר הנכנס שלך כדי לאשר את ההרשמה שלך.';
+$lang['Return to item page'] = 'חזור לדף הפריט';
+$lang['Stop receiving notifications'] = 'הפסק לקבל התראות';
+$lang['Subject'] = 'נושא';
+$lang['Subscribe'] = 'להירשם';
+$lang['Subscribe to mail notifications'] = 'להירשם להתראות בדואר';
+$lang['Subscribe without commenting'] = 'להירשם ללא להגיב';
+$lang['Subscriptions of %s'] = 'הרשמות של %s';
+$lang['Successfully unsubscribed your email address from receiving notifications.'] = 'ביטלת בהצלחה את כתובת הדוא"ל שלך מלקבל התראות.';
+$lang['To activate, click the confirm button. If you believe this is an error, please just ignore this message.'] = 'כדי להפעיל, לחץ על כפתור האישור. אם אתה חושב שזה שגיאה, אנא התעלם מהודעה זו.';
+$lang['Unsubscribe'] = 'ביטול הרשמה';
+$lang['Unsubscribe from all email notifications'] = 'בטל את כל ההתראות בדואר';
+$lang['Validate'] = 'אימות';
+$lang['Want to edit your notifications options?'] = 'רוצה לערוך את אפשרויות ההתראות שלך?';
+$lang['You are currently subscribed to comments on %s.'] = 'אתה רשום כעת לתגובות על %s.';
+$lang['You are not subscribed to any comment.'] = 'אינך רשום לשום תגובה.';
+$lang['You have been added to the list of subscribers.'] = 'הוספת לרשימת המנויים.';
+$lang['You requested to subscribe by email to comments on %s.'] = 'ביקשת להירשם באמצעות דוא"ל לתגובות על %s.';
+$lang['Your subscription has been validated, thanks you.'] = 'ההרשמה שלך אומתה, תודה לך.';
+$lang['all albums of the gallery'] = 'כל האלבומים בגלריה';
+$lang['all pictures of the album <a href="%s">%s</a>'] = 'כל התמונות באלבום <a href="%s">%s</a>';
+$lang['all pictures of the gallery'] = 'כל התמונות בגלריה';
+$lang['all pictures of this album'] = 'כל התמונות באלבום הזה';
+$lang['comments on a picture'] = 'תגובות על תמונה';
+$lang['comments on all pictures of an album'] = 'תגובות על כל התמונות באלבום';
+$lang['comments on an album'] = 'תגובות על אלבום';
+$lang['the album <a href="%s">%s</a>'] = 'האלבום <a href="%s">%s</a>';
+$lang['the picture <a href="%s">%s</a>'] = 'התמונה <a href="%s">%s</a>';
+$lang['this album'] = 'האלבום הזה';
+$lang['this picture'] = 'התמונה הזאת';
